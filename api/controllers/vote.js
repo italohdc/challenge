568c88f780b957a8af473472da55a6cd8c4fa9c2
Italo Lelis <italohdc@gmail.com>
2020-02-03 05:51:47 -0300

    Controller to register music votes

---
+++ api/controllers/vote.js
@@ -0,0 +1,28 @@
+const userService = require('../services/user');
+const voteService = require('../services/vote');
+const musicService = require('../services/music');
+
+module.exports = {
+
+  registerVote: async (req, res) => {
+    const { nickname, musics } = req.body;
+    if (!nickname) return res.status(400).send({ msg: "Parameter 'nickname' not provided" });
+    if (!musics) return res.status(400).send({ msg: "Parameter 'music' not provided" });
+    if (!Array.isArray(musics)) return res.status(400).send({ msg: "Parameter 'music' must be an array" });
+
+    const searchedUser = await userService.findByNickname(nickname);
+    if (searchedUser && searchedUser.hasVoted) return res.status(409).send({ msg: 'User has already voted' });
+
+    const newUser = await userService.put({ nickname, hasVoted: false });
+
+    const votePromises = musics.map((music) => voteService.put(newUser, { id: music }));
+    const incrementPromises = musics.map((music) => musicService.incrementVote({ id: music }));
+
+    Promise.all([...votePromises, ...incrementPromises]);
+
+    await userService.put({ nickname, hasVoted: true });
+
+    return res.status(200).send({ msg: 'Votes computed' });
+  },
+
+};
--- api/routes/vote.js
+++ api/routes/vote.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const voteController = require('../controllers/vote');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
-});
+router.post('/', voteController.registerVote);
 
 module.exports = router;
